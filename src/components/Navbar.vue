9eeadc98f4bbdd39e87b8770f1f67cf78a7c9fc2
PichardoWilfred <pichardowilfred@gmail.com>
2023-06-08 19:57:50 -0400

    navbar finished

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,11 @@
 
 <template>
-    <header class="sticky top-0 bg-white px-10 flex items-center w-full font-consolas font-light border-b border-gray-3 transition-all" :class="scrolled ? 'py-4':'py-5'">
+    <header class="sticky top-0 bg-white px-10 flex items-center w-full font-consolas font-light border-b border-gray-3 transition-all" :class="scrolled ? 'py-2':'py-4'">
         
-        <h3 v-if="scrolled" class="cursor-pointer font-consolas font-bold uppercase text-gray-8 text-3xl transition-all">
+        <h3 v-if="scrolled" class="cursor-pointer my-2 font-consolas font-bold uppercase text-gray-8 text-3xl transition-all">
             Hemiblade software
         </h3>
-        <img v-else alt="Hemiblade logo" class="cursor-pointer logo my-2 transition-all" src="@/assets/nav-logo.png" width="150" height="80" />
+        <img v-else alt="Hemiblade logo" class="cursor-pointer logo transition-all" src="@/assets/nav-logo.png" width="150" height="80" />
         
     
         <nav class="my-1 ms-auto text-lg transition-all h-full">
