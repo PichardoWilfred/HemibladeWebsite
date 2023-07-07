8fdc8add5cbb825faaf3f8e60907d8a01478cf51
PichardoWilfred <pichardowilfred@gmail.com>
2023-07-07 15:39:44 -0400

    fixed IFA typo in router.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/home/Home.vue';
 import Privacy from '../views/privacy/Privacy.vue';
 
-import Ifa from '../views/ifa/Ifa.vue';
+import Ifa from '../views/ifa/IFA.vue';
 import Highlights from '../views/ifa/highlights/Highlights.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
